b463256b575e866cb76cb0facc23a54131aecd09
Ramsey Gurley <ramseygurley@gmail.com>
2026-07-18 16:17:53 -0500

    Add logging to app archetype

--- webobjects-app-archetype/src/main/resources/archetype-resources/Sources/__packageInPathFormat__/Application.java
+++ webobjects-app-archetype/src/main/resources/archetype-resources/Sources/__packageInPathFormat__/Application.java
@@ -31,6 +31,20 @@ public class Application extends ERXApplication {
 	@Override
 	public void finishInitialization() {
 		super.finishInitialization();
+
+		/*
+		 * log4j2 reads its config out of log4j2.properties instead of the system
+		 * properties. The pom file places our Resources/log4j2.properties in the root
+		 * of target/classes so it will work. However, we want to read our logger
+		 * pattern from the wonder properties, so we reference a system property in the
+		 * log4j2.properties for the appender pattern. Unfortunately because of load
+		 * order, log4j2.properties are loaded before Wonder's ERXProperties. Therefore,
+		 * we call reconfigure here to reset the logging properties after we finish
+		 * loading everything in wonder.
+		 */
+		LoggerContext.getContext(false).reconfigure();
+		LOG.info("logger reconfigured");
+		
 		ERXPatcher.setClassForName(Main.class, Main.class.getSimpleName());
 
 		NSNotificationCenter.defaultCenter().addObserver(this,
